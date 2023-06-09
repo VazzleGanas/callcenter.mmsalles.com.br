86e89a18285fa6c4dc74514818968fc20cd45e8d
VazzleGanas <136104465+VazzleGanas@users.noreply.github.com>
2023-06-09 23:21:00 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<html> 
+<head> 
+<title>Hacked By Vazzle07 Ft Dea</title>
+<meta name="description" content="wH00PZ">
+<meta name="keywords" content="wH00PZ">
+<link href="https://fonts.googleapis.com/css?family=VT323&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Inconsolata&display=swap" rel="stylesheet">
+<meta property="og:image" content="https://i.ibb.co/rbgVCxH/c97c5972f077a68c90b33eaae0862e25.jpg">
+<link rel="Shortcut Icon" href="https://a.top4top.io/p_2083xd2kl0.jpg" type="icon">
+</head>
+<body bgcolor="black" onclick="play()">
+<table height="100%" width="100%"> 	
+<td align="center">
+<img src="https://a.top4top.io/p_2083xd2kl0.jpg" style="width: 400px;">
+<br><br>
+<font color="red" size="6">&#10017;<font face="VT323" color="red" size="6">Hacked By Vazzle07 Ft Dea</font>
+<br><br>
+<font face="Inconsolata" color="white" size="4">I am always waiting for you</font>
+<br>
+<font face="Inconsolata" color="white" size="4">Greetz:</font>
+<br>
+<font face="Inconsolata" color="red" size="4"><i>- T.ME/LEAKSTOOLS - DEA -<font color="white">- </i>
+<br><br><br>
+<audio controls>ï»¿     
+<source src="https://j.top4top.io/m_2716061im1.mp3" type="audio/mpeg">
+</audio>
